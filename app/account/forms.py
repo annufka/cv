2377b3d8b8d082e4ceb479ccae1237f5770b2442
Ann <31072029+annufka@users.noreply.github.com>
2021-09-11 16:27:51 +0300

    bootstrap

--- app/account/forms.py
+++ app/account/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
