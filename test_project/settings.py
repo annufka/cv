a48c3208dff4714568e73e1c4d3752a9d713d956
Ann <31072029+annufka@users.noreply.github.com>
2021-09-17 09:08:54 +0300

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -138,3 +138,6 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
