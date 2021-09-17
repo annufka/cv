c13a6f7776864336d4fdd19ef282f16c7a6c4add
Ann <31072029+annufka@users.noreply.github.com>
2021-09-17 11:08:16 +0300

    Update models.py

--- app/account/models.py
+++ app/account/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Schema(models.Model):
